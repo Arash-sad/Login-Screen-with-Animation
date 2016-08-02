19ac8d64d6182fb4d02081d11b25da578e5b64bb
DrArash <arash_part2@yahoo.com>
2016-08-02 15:17:17 +1000

    Initial Commit

---
+++ LoginScreenWithAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoginScreenWithAnimation
+//
+//  Created by Arash Sadeghieh E on 2/08/2016.
+//  Copyright © 2016 Treepi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
